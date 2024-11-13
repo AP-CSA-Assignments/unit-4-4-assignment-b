deb2fae8f9fc979307df4e6ece7804c56859695f
Kyle Vu <ktvu@hbuhsd.edu>
2024-11-12 22:25:51 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,4 +6,19 @@ public class Main
   {
     // Put your solutions below
   }
+
+  public static int countSub(String word, String target)
+  {
+    return 0;
+  }
+
+  public static int countProperContains(String word, String target)
+  {
+    return 0;
+  }
+
+  public static String deleteSub(String word, String target)
+  {
+    return null;
+  }
 }
